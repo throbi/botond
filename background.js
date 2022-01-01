55a0cff89ca2e4d119ca29bea9cf1ed6e12e1a14
Robert Horvath <robi@mmm.local>
2022-01-01 23:11:12 +0100

    alert on clicking the extension's button

---
+++ background.js
@@ -0,0 +1,7 @@
+// install action to be executed when the extension button is clicked
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    files: ['content.js']
+  });
+});
